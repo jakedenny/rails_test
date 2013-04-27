7e4eb5d60d1e947f856030746caa29de9cf9b80e
Jake Denny <jake.m.denny@gmail.com>
2013-04-27 15:54:57 -0400

    Add user form to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,40 @@ class StaticPagesController < ApplicationController
 	
 	def welcome
 		@math = 10*10
+		@last_user_name = User.last.name
+		@last_user_email = User.last.email
+
+		@new_user = User.new
+	end
+
+
+	def create
+		# good way to debug what is being passed in
+		#puts "*****************"
+		#puts params[:user]
+		#puts "*****************"
+
+		@new_user = User.new(params[:user])
+		if @new_user.save
+			flash[:success] = "Welcome home young one. Welcome."
+			redirect_to root_url
+		else
+			flash[:error] = "This failed. What a tragedy. Eat a banana."
+			redirect_to root_url
+		end
 	end
 
 end
+
+
+
+#def create
+#    @client = Client.new(params[:client])
+#    if @client.save
+#      redirect_to @client
+#    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+#      render :action => "new"
+#    end
+#  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 RailsTest::Application.routes.draw do
   root :to => "static_pages#welcome"
-
   #get "/" => "static_pages#welcome"
+
+  post "create" => "static_pages#create"
+
+  #No route matches {:action=>"create", :controller=>"static_pages"}
   
 end
